035d9f114ac9abdb9d7789f84d3a131e7f7edba6
Caleb Taylor <caleb.taylor@nike.com>
2015-08-18 16:54:55 -0700

    First failed test for shield

---
+++ source/TestedTrekLab/StarTrek/subsystems/shields.js
@@ -0,0 +1,4 @@
+function Shield() {
+    this.energy = 4000;
+};
+
--- source/TestedTrekLab/Tests/subsystems/shieldsSpec.js
+++ source/TestedTrekLab/Tests/subsystems/shieldsSpec.js
@@ -1,9 +1,11 @@
 describe("Shields", function() {
 
-
   beforeEach(function() {
-
   });
 
+  it("tests if shiels are down by default", function() {
+    var shield = new Shield();
+    expect(shield.isUp()).toBe(true);
+  });
 
 });
