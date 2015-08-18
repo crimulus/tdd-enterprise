caacb6b08e56a9722f5324fc06def4ddfb1919d5
Bruckner, Aaron (ETW - FLEX Resource) <Aaron.Bruckner@nike.com>
2015-08-18 16:38:34 -0700

    First commit example

---
+++ source/TestedTrekLab/Tests/exampleSpec.js
@@ -0,0 +1,14 @@
+describe("example", function() {
+
+
+  beforeEach(function() {
+
+  });
+
+  it("should pass with example.js", function() {
+    var example = new Example();
+
+    expect(example.test).toBeTruthy();
+  });
+
+});
