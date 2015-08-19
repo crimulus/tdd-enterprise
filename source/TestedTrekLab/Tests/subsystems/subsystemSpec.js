cc42108209ebcbd92c07c1756044f661aac36f9b
unknown <mmil31@LN00HAKW4E5040.ad.nike.com>
2015-08-19 09:51:08 -0700

    Added damage test.

--- source/TestedTrekLab/Tests/subsystems/subsystemSpec.js
+++ source/TestedTrekLab/Tests/subsystems/subsystemSpec.js
@@ -6,12 +6,18 @@ describe("Subsystem", function() {
         subsystem = new Subsystem();
     });
 
-    it("tests if subsystem is not dammaged by default", function() {
+    it("tests if subsystem is not damaged by default", function() {
         expect(subsystem.isDamaged()).toBe(false);
     });
 
     it("tests how many days for subsystem to recover, 0 by default", function() {
         expect(subsystem.daysToRecover()).toBe(0);
     });
+
+    it("tests if subsystem is damaged", function() {
+        subsystem.numberOfDaysToRecover = 1;
+        expect(subsystem.isDamaged()).toBe(true);
+    });
+
 
 });
