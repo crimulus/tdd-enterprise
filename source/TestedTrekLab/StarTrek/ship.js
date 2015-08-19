065014a6b2c9387ac91fd35c6446f7d5e4e183b6
Tharp, Jeremy <Jeremy.Tharp@nike.com>
2015-08-19 10:05:00 -0700

    Resting logic Ship energy transfer stub

--- source/TestedTrekLab/StarTrek/ship.js
+++ source/TestedTrekLab/StarTrek/ship.js
@@ -1,8 +1,30 @@
-function Ship(){
+function Ship(game){
+
   var self = this;
 
   //Variables
   self.energyReserves = 20000;
+  self.subsystems = [];
+
+  self.addSubsystem = function (subsystem) {
+    if (subsystem.constructor.name !== 'Subsystem') {
+      throw new Error('Passed subsystem is not a subsystem');
+    }
+    self.subsystems.push(subsystem);
+  };
+
+  self.takeEnergy = function (amount) {
+    return amount;
+  };
+
+/*
+  game.addRestListener(function () {
+    for (var i in self.subsystems) {
+      var subsystem = self.subsystems[i];
+    }
+  });
+*/
 
   return self;
+
 }
--- source/TestedTrekLab/Tests/shipSpec.js
+++ source/TestedTrekLab/Tests/shipSpec.js
@@ -10,4 +10,34 @@ describe("ship", function() {
     expect(ship.energyReserves).toEqual(20000);
   });
 
+  it("should have an array of subsystems", function() {
+    expect(ship.subsystems.length).toBe(0);
+  });
+
+  it("should expose a method to add new subsystems", function () {
+    expect(typeof ship.addSubsystem).toBe('function');
+  });
+
+  it("should add a subsystem to the array of subsystems", function () {
+    var subsystem = new Subsystem();
+    ship.addSubsystem(subsystem);
+    expect(ship.subsystems.length).toBe(1);
+    expect(ship.subsystems.indexOf(subsystem)).toBe(0);
+  });
+
+  it("should throw an error if I try to add a subsystem that is not a subsystem", function () {
+    var subsystemDecoy = 'Spock wuz here';
+    expect(function () {
+      ship.addSubsystem(subsystemDecoy);
+    }).toThrow();
+  });
+
+  it("should expose a method to transfer energy to a subsystem", function () {
+    expect(typeof ship.takeEnergy).toBe('function');
+  });
+
+  it("should return the amount of energy I requested", function () {
+    expect(ship.takeEnergy(500)).toBe(500);
+  });
+
 });
