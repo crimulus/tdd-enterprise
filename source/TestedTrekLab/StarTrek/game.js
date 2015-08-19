f02081c22b410d08ba8d2e8e18d92a2c7a5309c4
Tharp, Jeremy <Jeremy.Tharp@nike.com>
2015-08-19 09:42:59 -0700

    Adding rest callback functionality

--- source/TestedTrekLab/StarTrek/game.js
+++ source/TestedTrekLab/StarTrek/game.js
@@ -1,13 +1,23 @@
 function Game(){
 
   var self = this;
+  var restListeners = [];
 
   self.lapsedTime = 0;
-  self.subsystems = [];
 
-  self.rest = function (days) {
-    self.lapsedTime += days;
-  }
+  self.rest = function (daysToRest) {
+    self.lapsedTime += daysToRest;
+    for (var i in restListeners) {
+      restListeners[i]();
+    };
+  };
+
+  self.addRestListener = function (callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('callback passed was not a function.');
+    }
+    restListeners.push(callback);
+  };
 
   // Variables
   return self;
--- source/TestedTrekLab/Tests/gameSpec.js
+++ source/TestedTrekLab/Tests/gameSpec.js
@@ -15,8 +15,23 @@ describe("game", function() {
     expect(game.lapsedTime).toBe(3);
   });
 
-  it("should have an empty array of subsystems", function() {
-    expect(game.subsystems.length).toBe(0);
+  it("should have a function to add rest listeners", function () {
+    expect(typeof game.addRestListener).toBe('function');
+  });
+
+  it("should throw an error if an attempt to add a listener that is not a function is made", function () {
+    expect(function () {
+      game.addRestListener("not a function")
+    }).toThrow();
+  });
+
+  it("should call my rest callback when resting", function () {
+    var called = false;
+    game.addRestListener(function () {
+      called = true;
+    });
+    game.rest(3);
+    expect(called).toBe(true);
   });
 
 });
